0a4998f70fa003f31a95527943bef9e941b622b4
hyeon12 <seojh912_@naver.com>
2024-06-26 16:20:15 +0900

    관리자/회원 접근 권한 분리

--- src/main/java/org/choongang/admin/advice/AdminControllerAdvice.java
+++ src/main/java/org/choongang/admin/advice/AdminControllerAdvice.java
@@ -1,13 +1,24 @@
 package org.choongang.admin.advice;
 
+import lombok.RequiredArgsConstructor;
+import org.choongang.global.Interceptor;
 import org.choongang.global.config.annotations.ControllerAdvice;
-import org.choongang.global.config.annotations.ModelAttribute;
+import org.choongang.global.exceptions.UnAuthorizedException;
+import org.choongang.member.MemberUtil;
 
+@RequiredArgsConstructor
 @ControllerAdvice("org.choongang.admin")
-public class AdminControllerAdvice {
-    @ModelAttribute
-    public String testValue(){
-        System.out.println("testValue!");
-        return "test";
+public class AdminControllerAdvice implements Interceptor {
+
+    private final MemberUtil memberUtil;
+
+    @Override
+    public boolean preHandle() {
+
+        if(!memberUtil.isAdmin()){
+            throw new UnAuthorizedException();//관리자 아닐땐 401 페이지 출력
+        }
+
+        return true;
     }
 }
---
+++ src/main/webapp/WEB-INF/templates/board/notice.jsp
@@ -0,0 +1,8 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<%@ taglib prefix="fmt" uri="jakarta.tags.fmt" %>
+<%@ taglib prefix="layout" tagdir="/WEB-INF/tags/layouts" %>
+<fmt:setBundle basename="messages.commons" />
+<fmt:message var="pageTitle" key="공지사항" />
+
+<h1>${pageTitle}</h1>
