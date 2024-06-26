d6c6cff56113d773b7899ded0a564dc50ddcd30a
hyeon12 <seojh912_@naver.com>
2024-06-26 11:00:31 +0900

    로그인 검증 과정

--- src/main/java/org/choongang/member/controllers/MemberController.java
+++ src/main/java/org/choongang/member/controllers/MemberController.java
@@ -7,6 +7,7 @@ import org.choongang.global.config.annotations.GetMapping;
 import org.choongang.global.config.annotations.PostMapping;
 import org.choongang.global.config.annotations.RequestMapping;
 import org.choongang.member.services.JoinService;
+import org.choongang.member.services.LoginService;
 
 @Controller
 @RequestMapping("/member")//앞의 주소 member 로 맵핑
@@ -14,6 +15,7 @@ import org.choongang.member.services.JoinService;
 public class MemberController {
 
     private final JoinService joinService;
+    private final LoginService loginService;
 
     //회원 가입 양식
     @GetMapping("/join")
@@ -44,8 +46,11 @@ public class MemberController {
     //로그인 처리
     @PostMapping("/login")
     public String loginPs(RequestLogin form){
-        System.out.println(form);
-        return null;
+        loginService.process(form);
+        return "commons/execute_script";
+        //html 페이지가 렌더링 되어야만 스크립트 태그를 만나 스크립트가 실행되는 것
+        //웹브라우저에서 스크립트를 실행하려면 스크립트 태그가 필요함
+
     }
 }
 
