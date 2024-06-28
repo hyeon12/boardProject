cecc7f824cd2a031619f5c8fcf846de145d03761
hyeon12 <seojh912_@naver.com>
2024-06-28 18:39:21 +0900

    게시판 공통 css, js 설정

--- src/main/java/org/choongang/board/entities/Board.java
+++ src/main/java/org/choongang/board/entities/Board.java
@@ -4,6 +4,10 @@ import lombok.Builder;
 import lombok.Data;
 import org.choongang.board.constants.Authority;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @Data
 @Builder
 public class Board {
@@ -14,4 +18,18 @@ public class Board {
     private int activeCategory;
     private String category;
     private Authority authority; //ENUM 클래스 상수 사용하기 위함
+
+    //분류 목록
+    public List<String> getCategories(){
+        if(category != null){
+
+            List<String> categories = Arrays.stream(category.trim().split("\\n"))
+                    .map(s -> s.replace("\\r", ""))
+                    .toList();
+
+            return categories;
+        }
+
+        return Collections.EMPTY_LIST; //오류 방지 (NullPointerException)
+    }
 }
---
+++ src/main/webapp/WEB-INF/templates/board/_header.jsp
@@ -0,0 +1,12 @@
+<%-- board 쪽 header 영역에 삽입될 내용 --%>
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<c:url var="mainUrl" value="/" />
+<c:url var="listUrl" value="/board/list/${board.BId}" />
+<div class="location">
+    <a href="${mainUrl}">HOME</a>
+    &gt;
+    <a href="${listUrl}">${board.BName}</a>
+</div>
+
+<h1>${board.BName}</h1>
